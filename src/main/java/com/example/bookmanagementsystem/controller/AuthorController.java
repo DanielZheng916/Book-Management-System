37faad4e13fc119740fa8736cdc869bde183961d
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-05-04 11:07:12 -0400

    Add Kafka Producer

--- src/main/java/com/example/bookmanagementsystem/controller/AuthorController.java
+++ src/main/java/com/example/bookmanagementsystem/controller/AuthorController.java
@@ -3,23 +3,38 @@ package com.example.bookmanagementsystem.controller;
 import com.example.bookmanagementsystem.exception.AuthorNotFoundException;
 import com.example.bookmanagementsystem.model.Author;
 import com.example.bookmanagementsystem.repository.AuthorRepository;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
 
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     private AuthorRepository repository;
 
-    public AuthorController(AuthorRepository repository) {
+    @Value(value = "${author.topic.name}")
+    private String topicAuthor;
+
+    public AuthorController(AuthorRepository repository, KafkaTemplate<String, String> kafkaTemplate) {
         this.repository = repository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/")
     public Author create(@RequestBody Author newAuthor) {
-        return repository.save(newAuthor);
+        Author createdAuthor = repository.save(newAuthor);
+        String message = "Create author: " + createdAuthor;
+        sendMessage(topicAuthor, message);
+        return createdAuthor;
     }
 
     @GetMapping("/")
@@ -48,5 +63,18 @@ public class AuthorController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    private void sendMessage(String topicName, String message) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+        future.whenComplete((result, ex) -> {
+
+            if (ex == null) {
+                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata()
+                        .offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+            }
+        });
     }
 }
