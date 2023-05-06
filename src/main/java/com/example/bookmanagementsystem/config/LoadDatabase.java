c127886c41b951f740053f794558e76f27ce91e9
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-05-06 14:54:51 -0400

    Config deletes all books data when starting the project.

--- src/main/java/com/example/bookmanagementsystem/config/LoadDatabase.java
+++ src/main/java/com/example/bookmanagementsystem/config/LoadDatabase.java
@@ -2,6 +2,7 @@ package com.example.bookmanagementsystem.config;
 
 import com.example.bookmanagementsystem.model.Author;
 import com.example.bookmanagementsystem.repository.AuthorRepository;
+import com.example.bookmanagementsystem.repository.BookRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -13,8 +14,9 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(AuthorRepository repository) {
+    CommandLineRunner initDatabase(AuthorRepository repository, BookRepository bookRepository) {
 
+        bookRepository.deleteAll();
         repository.deleteAll();
 
         return args -> {
